25d49fc48d20035a2946d8767edbdd1e280b08e6
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-11 14:15:06 -0500

    fixed responsiveness mainFeatures.jsx

--- src/Components/MainFeatures.jsx
+++ src/Components/MainFeatures.jsx
@@ -22,17 +22,17 @@ export default function MainFeatures() {
                     </div>
                     <div className='flex gap-x-2 lg:flex-row flex-col'>
                         <div className="w-full lg:w-1/4 items-center lg:items-end gap-8 justify-center lg:justify-around flex flex-col md:flex-row lg:flex-col">
-                            <div className='flex flex-col gap-1 w-max'>
+                            <div className='flex flex-col gap-1 w-9/12'>
                                 <ForwardIcon className='h-10 w-10 p-2 bg-green-200 text-green-700 rounded-full'/>
                                 <div className="text-lg font-bold font-outfit tracking-wide ">Available <span className="main-gradient font-bold">Everywhere</span></div>
-                                <div className="text-md w-80 font-outfit">
+                                <div className="text-md font-outfit">
                                     EcoEats is available on both IOS and Android, allowing anyone to download and contribute! 
                                 </div>
                             </div>
-                            <div className='flex flex-col gap-1 h-max '>
+                            <div className='flex flex-col gap-1 w-9/12 h-max'>
                                 <ForwardIcon className='h-10 w-10 p-2 bg-green-200 text-green-700 rounded-full'/>
                                 <div className="text-lg font-bold font-outfit tracking-wide ">Contribute <span className="main-gradient font-bold">Easier.</span></div>
-                                <div className="text-md w-80 font-outfit">
+                                <div className="text-md font-outfit">
                                     Whether you're a student or an adult, EcoEats allows anyone to contribute to their local community
                                 </div>
                             </div>
@@ -41,17 +41,17 @@ export default function MainFeatures() {
                             <img src={featuresshot} alt="features shot app mockup" className='w-full' />
                         </div>
                         <div className="w-full lg:w-1/4 items-center gap-8 lg:items-start justify-center lg:justify-around flex flex-col md:flex-row lg:flex-col">
-                            <div className='flex flex-col gap-1 w-max'>
+                            <div className='flex flex-col gap-1 w-9/12'>
                                 <ForwardIcon className='h-10 w-10 p-2 bg-green-200 text-green-700 rounded-full'/>
                                 <div className="text-lg font-bold font-outfit tracking-wide ">Donate <span className="main-gradient font-bold">Easier</span></div>
-                                <div className="text-md w-80 font-outfit">
+                                <div className="text-md font-outfit">
                                     Easily sign-up up to be a donor on our platform and post listings allowing recipients to view and claim! 
                                 </div>
                             </div>
-                            <div className='flex flex-col gap-1 w-max '>
+                            <div className='flex flex-col gap-1 w-9/12 '>
                                 <ForwardIcon className='h-10 w-10 p-2 bg-green-200 text-green-700 rounded-full'/>
                                 <div className="text-lg font-bold font-outfit tracking-wide ">Recieve <span className="main-gradient font-bold">Easier</span></div>
-                                <div className="text-md w-80 font-outfit">
+                                <div className="text-md font-outfit">
                                     Recipients can easily view and claim listings near them and have a volunteer pick them up
                                 </div>
                             </div>
