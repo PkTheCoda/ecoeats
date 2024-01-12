84075335edf18b6318851870e2d7a0155378c00a
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-12 07:43:28 -0500

    fixed spelling errors, done FR!

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -55,7 +55,7 @@ export default function Testimonials() {
                 <div className="bg-slate-100 w-full py-10">
                 <div className="mx-auto w-full px-6 lg:px-8">
                     <h2 className="text-center text-lg font-semibold leading-8 text-gray-700 font-outfit">
-                        Trusted and Support by:
+                        Trusted and Supported by:
                     </h2>
                     <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                     <img
