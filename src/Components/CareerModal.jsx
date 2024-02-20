b0662594ac65cebe98a3708b876a1231502c94a9
PkTheCoda <pranavkonjeti@gmail.com>
2024-02-20 18:27:58 -0500

    changed modal button

--- src/Components/CareerModal.jsx
+++ src/Components/CareerModal.jsx
@@ -177,10 +177,10 @@ export default function CareerModal({open, setOpen, activeJob}) {
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md transition300 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md transition300 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
                     onClick={() => {setOpen(false);}}
                   >
-                    Remove
+                    Submit
                   </button>
                   <button
                     type="button"
